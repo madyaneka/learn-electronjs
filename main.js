1b29abe6420d4ee6c6463b67abd8797d630b7840
Madyan Eka <madyan.eka95@gmail.com>
2018-10-05 09:33:12 +0700

    create first window, fix npm start script

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log(`I am Madyan.`)
+const { app, BrowserWindow } = require('electron')
+
+let createWindow = () => {
+
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  // Load the index.html of the app.
+  win.loadFile('index.html')
+
+}
+
+app.on('ready', createWindow)
